43fd8d3967afe8829c55b2f3ca0d4aacf4e303b8
oriana marquina <omarquina@pronovasalud.com>
2024-08-07 16:30:19 -0400

    roles y permisos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +18,22 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
+            'name' => 'admin',
+            'email' => 'testadmin@gmail.com',
+        ]);
+
+        User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        // Crear roles y permisos
+        $role = Role::create(['name' => 'admin']);
+        $permission = Permission::create(['name' => 'edit articles']);
+
+        // Asignar rol y permiso a un usuario
+        $user = User::find(1); // Obtén el usuario por ID
+        $user->assignRole('admin');
+        $user->givePermissionTo('edit articles');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group(['middleware' => ['role:admin']], function() {
+    //Route::get('/admin', [AdminController::class, 'index']);
+});
