29b8124adccecb1bc6c8b6596035744829b8e5d8
oriana marquina <omarquina@pronovasalud.com>
2024-10-22 15:47:49 -0300

    fixes

--- app/Http/Controllers/Pages/PagesController.php
+++ app/Http/Controllers/Pages/PagesController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Pages;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class Menu extends Controller
+class PagesController extends Controller
 {
     /**
      * Display a listing of the resource.
