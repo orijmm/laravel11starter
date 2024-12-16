36a588fb8e6eee2e65e9c147dc4e47c537a332b0
oriana marquina <omarquina@pronovasalud.com>
2024-12-16 10:13:08 -0300

    add aos animation

--- resources/app/main.js
+++ resources/app/main.js
@@ -1,13 +1,15 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import router from "@/router";
 import i18n from "@/plugins/i18n";
 import App from "@/App";
 import mdiPlugin from "@/plugins/mdicons"; // Ajusta la ruta a donde tengas `mdi.js`
-
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 const app = createApp(App)
+    .use(AOS.init());
 
 app.use(createPinia());
 
