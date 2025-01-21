7a71275c0024e2f3132714886bf06faddb5eea41
oriana marquina <omarquina@pronovasalud.com>
2025-01-21 10:47:41 -0300

    mostrar clases en página

--- resources/app/views/pages/public/home/Content.vue
+++ resources/app/views/pages/public/home/Content.vue
@@ -2,8 +2,8 @@
     <div class="w-full">
         <!-- sections -->
         <section v-for="(section, i) in page.sections" :id="`section-${section.name}`" :class="section.classes">
-            <div v-for="row in section.rows" class="grid grid-flow-col">
-                <div v-for="col in row.columns">
+            <div v-for="row in section.rows" class="grid grid-flow-col" :class="row.classes">
+                <div v-for="col in row.columns" :class="col.classes">
                     <div v-for="component in col.components">
                         <component :is="component.componenttype.name || 'div'" :content="component.contents">
                             {{ component.componenttype.name ? '' : 'Componente no encontrado' }}
@@ -12,32 +12,6 @@
                     </div>
                 </div>
             </div>
-        </section>
-
-        <!-- Credit card section -->
-        <section class="w-full my-36">
-            <BaseSection data-aos="fade-down">
-                <div class="col-span-12 lg:col-span-7">
-                    <div class="w-full">
-                        <img src="@/views/pages/public/template/assets/img/nefa-cc.webp" class="w-[95%]" alt="" />
-                    </div>
-                </div>
-                <div class="col-span-12 lg:col-span-5 space-y-6 px-4 sm:px-6 mt-20">
-                    <h2 class="text-4xl font-semibold">
-                        Introducing the <span class="text-header-gradient">NEFA</span> Credit Card
-                    </h2>
-                    <p class="paragraph">Subject to cardholder and rewards terms which will be available at application.
-                    </p>
-                    <ul class="space-y-4 sm:space-y-2">
-                        <LandingListItem title="Up to 3% back on purchases" />
-                        <LandingListItem title="Earn rewards in bitcoin or any crypto on NEFA" />
-                        <LandingListItem title="No annual fee" />
-                    </ul>
-                    <BaseButton
-                        class="w-full sm:max-w-[240px] px-10 py-4 bg-inherit text-gradient border border-[#0c66ee] text-base">
-                        Join the waitlist</BaseButton>
-                </div>
-            </BaseSection>
         </section>
 
         <!-- Advanced trading tools section -->
