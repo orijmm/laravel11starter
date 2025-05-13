729c10b46219d1698d4a2704d4a9706bb24d19b6
oriana marquina <omarquina@pronovasalud.com>
2025-05-13 11:52:38 -0400

    socials

--- resources/app/views/pages/public/Template/components/base/Socials.vue
+++ resources/app/views/pages/public/Template/components/base/Socials.vue
@@ -1,26 +1,35 @@
 <template>
   <!-- Social media links using map -->
-  <a v-for="(socialMedia, index) in socialMediaLinks" :key="index" :href="socialMedia.href">
-    <i :class="socialMedia.iconClass"></i>
+  <a v-if="webdata.twitter" target="_blank" :href="webdata.twitter ?? '#'">
+    <i class="uil uil-twitter"></i>
+  </a>
+  <a v-if="webdata.facebook" target="_blank" :href="webdata.facebook ?? '#'">
+    <i class="uil uil-facebook-f"></i>
+  </a>
+  <a v-if="webdata.instagram" target="_blank" :href="webdata.instagram ?? '#'">
+    <i class="uil uil-instagram"></i>
+  </a>
+  <a v-if="webdata.youtube" target="_blank" :href="webdata.youtube ?? '#'">
+    <i class="uil uil-youtube"></i>
   </a>
 </template>
 
 <script>
-export default {
-  props: ['webdata'],
-  setup(props) {
-    console.log(props);
-    const socialMediaLinks = [
-      { id: 1, href: props.webdata.twitter ?? "#", iconClass: "uil uil-twitter" },
-      { id: 2, href: "#", iconClass: "uil uil-facebook-f" },
-      { id: 3, href: "#", iconClass: "uil uil-dribbble" },
-      { id: 4, href: "#", iconClass: "uil uil-instagram" },
-      { id: 5, href: "#", iconClass: "uil uil-youtube" },
-    ];
-    return {
-      socialMediaLinks
-    }
-  }
-};
 
+export default {
+  props: {
+    webdata: {
+      type: Object,
+      required: true,
+      default: {},
+    },
+  }
+  // const socialMediaLinks = [
+  //     { id: 1, href: props.webdata.twitter ?? "#", iconClass: "uil uil-twitter" },
+  //     { id: 2, href: "#", iconClass: "uil uil-facebook-f" },
+  //     { id: 3, href: "#", iconClass: "uil uil-dribbble" },
+  //     { id: 4, href: "#", iconClass: "uil uil-instagram" },
+  //     { id: 5, href: "#", iconClass: "uil uil-youtube" },
+  //   ];
+};
 </script>
