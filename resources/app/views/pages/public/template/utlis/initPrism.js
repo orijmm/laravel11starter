39e9685c38a2e81c2b5b9a55f24f2d1aa7173858
oriana marquina <omarquina@pronovasalud.com>
2025-06-26 16:38:31 -0400

    test2

--- resources/app/views/pages/public/template/utlis/initPrism.js
+++
@@ -1,6 +0,0 @@
-import Prism from "prismjs";
-const initPrism = () => {
-  Prism.highlightAll();
-};
-
-export default initPrism;
--- resources/app/views/pages/public/template/utlis/injextSvg.js
+++
@@ -1,4 +0,0 @@
-export const injectSvg = async () => {
-  const SVGInject = (await import("@iconfu/svg-inject")).default;
-  SVGInject(document.getElementsByClassName("icon-svg"));
-};
