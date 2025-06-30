e2aca995b25444fe5af9a3c757da2720c8a6b5d4
oriana marquina <omarquina@pronovasalud.com>
2025-06-30 15:48:23 -0400

    testlog7

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -38,7 +38,7 @@ class SettingController extends Controller
     {
         try {
             $model = $this->settingService->get($settingad);
-            Log::infoo('lang:',['data' => $model]);
+            Log::info('lang:',['data' => $model]);
             return $this->responseDataSuccess(['model' => $model]);
         } catch (Exception $e) {
             // Error inesperado
