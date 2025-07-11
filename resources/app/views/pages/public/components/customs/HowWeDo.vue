18d1d50ebebf28749825c097c5578f3252638262
oriana marquina <omarquina@pronovasalud.com>
2025-07-11 16:52:32 -0400

    animaciones

--- resources/app/views/pages/public/components/customs/HowWeDo.vue
+++ resources/app/views/pages/public/components/customs/HowWeDo.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="container">
-      <div class="col-lg-6 position-lg-absolute top-0 start-0 image-wrapper bg-image bg-cover h-100"
+      <div data-aos="flip-right" data-aos-delay="200" class="col-lg-6 position-lg-absolute top-0 start-0 image-wrapper bg-image bg-cover h-100"
         :style="{ backgroundImage: `url(${img[0] ?? '/assets/img/photos/bg38.jpg'})` }">
         <div class="divider text-gray divider-v-end d-none d-lg-block">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 54 1200">
@@ -19,13 +19,14 @@
       <div class="row gx-0">
         <div class="col-lg-6 ms-auto">
           <div class="pt-13 pb-15 pb-md-17 py-lg-16 ps-lg-7 pe-xxl-4">
-            <h2 class="fs-15 text-uppercase text-muted mb-3">{{ content[0]?.text ??
+            <h2 class="fs-15 text-uppercase text-muted mb-3"  data-aos="flip-down" data-aos-delay="200">{{ content[0]?.text ??
                     trans('global.phrases.hasto_add_content') }}</h2>
-            <h3 class="display-3 ls-sm mb-7 mb-lg-10">
+            <h3 class="display-3 ls-sm mb-7 mb-lg-10"  data-aos="flip-down" data-aos-delay="200">
               {{ content[1]?.text ?? trans('global.phrases.hasto_add_content') }}
             </h3>
             <div class="row gy-8">
-              <div v-for="(service, i) in services" :key="service.id" class="col-12 col-lg-6 d-flex">
+              <div v-for="(service, i) in services" :key="service.id" class="col-12 col-lg-6 d-flex"
+              data-aos="flip-down" data-aos-once="true" :data-aos-delay="200 + i * 100">
                 <div>
                   <div class="icon-svg icon-svg-xs  me-5 mt-1" :class="service.icon_color_class"
                     :style="{ '-webkit-mask-image': `url(${service.icon})`, 'mask-image': `url(${service.icon})` }">
--- resources/app/views/pages/public/components/customs/WhatWeDo.vue
+++ resources/app/views/pages/public/components/customs/WhatWeDo.vue
@@ -2,25 +2,27 @@
     <div class="container py-15 py-md-17">
         <div class="row text-center py-4">
             <div class="col-11 col-lg-10 col-xxl-9 mx-auto">
-                <h3 class="display-3 ls-sm mb-9 px-xl-11">{{ content[0]?.text ??
-                    trans('global.phrases.hasto_add_content') }}</h3>
-                <h2 class="fs-15 text-uppercase text-muted mb-3">
+                <h3 class="display-3 ls-sm mb-9 px-xl-11" data-aos="fade-up" data-aos-once="true" data-aos-delay="200">
+                    {{ content[0]?.text ??
+                        trans('global.phrases.hasto_add_content') }}</h3>
+                <h2 class="fs-15 text-uppercase text-muted mb-3"  data-aos="fade-zoom-in" data-aos-once="true" data-aos-delay="300">
                     {{ content[1]?.text ?? trans('global.phrases.hasto_add_content') }}
                     <span class="underline-3 style-2 orange">{{ content[2]?.text ??
                         trans('global.phrases.hasto_add_content')
-                        }}&nbsp;</span>
+                    }}&nbsp;</span>
                     <span class="underline-3 style-2 orange">{{ content[3]?.text ??
                         trans('global.phrases.hasto_add_content')
-                        }}</span>
+                    }}</span>
                     {{ content[4]?.text ?? trans('global.phrases.hasto_add_content') }}
-                    <span class="underline-3 style-3 green">{{ content[5]?.text ?? trans('global.phrases.hasto_add_content') }}</span>
+                    <span class="underline-3 style-3 green">{{ content[5]?.text ??
+                        trans('global.phrases.hasto_add_content') }}</span>
                     {{ content[6]?.text ?? trans('global.phrases.hasto_add_content') }}
                 </h2>
             </div>
         </div>
         <div class="row gx-lg-8 gx-xl-12 gy-8">
             <div v-for="(service, i) in services" :key="service.id" class="col-md-6 col-lg-3">
-                <div class="d-flex flex-row">
+                <div class="d-flex flex-row" data-aos="fade-up" data-aos-once="true" :data-aos-delay="100 + i * 100">
                     <div>
                         <div class="icon-svg icon-svg-xs  me-5 mt-1" :class="service.icon_color_class"
                             :style="{ '-webkit-mask-image': `url(${service.icon})`, 'mask-image': `url(${service.icon})` }">
